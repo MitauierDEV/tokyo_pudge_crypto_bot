e36f0d5660b10eb5585a74d048e4c9f8cdde4d34
ilia.kopranov <ilia.kopranov@ringcentral.com>
2025-02-02 19:16:39 +0400

    get-prices: rename get_crypto_currency_price_by_date

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from crypto_client.crypto_currency_client import *
 
 def main():
-    price = get_crypto_currency_price('bitcoin', '29-12-2024')
+    price = get_crypto_currency_price_by_date('bitcoin', '29-12-2024')
     print(price.text)
 
 
